c9a45d5ef762b38da2722e38e3695e2fa5539262
unknown <deswitas@gmail.com>
2025-04-16 08:13:20 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_103022300058
+{
+    class Transfer
+    { 
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+    }
+
+    class Confirmation
+    { 
+        public string en { get; set; }
+        public string id { get; set; }
+    }
+    class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public Transfer transfer { get; set; }
+        public Confirmation confirmation { get; set; }
+        public List<string>methods { get; set; }
+
+        String path = Path.GetFullPath("D:\\mod8\\modul8_103022300058\\datas\\bank_transfer_config.json");
+
+        public BankTransferConfig LoadConf()
+        {
+            if (!File.Exists(path)) 
+            {
+                var defaultSetting = new BankTransferConfig();
+                SaveNewConfig(defaultSetting);
+                return defaultSetting;
+            }
+            string json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<BankTransferConfig>(json);
+
+        }
+
+        public void SaveNewConfig(BankTransferConfig config)
+        {
+            var json = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(path, json);
+        }
+
+        public void GantiBahasa()
+        {
+            if (lang.Equals("en"))
+            {
+                lang = "id";
+            }
+            else 
+            {
+                lang = "en";
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul8_103022300058;
+using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(String[] args) 
+    {
+        BankTransferConfig pengaturan = new BankTransferConfig().LoadConf();
+        Console.Write("Ingin mengubah bahasa? (y/n): ");
+        string input = Console.ReadLine();
+
+        int uang;
+
+        if (input.ToLower() == "y")
+        {
+            pengaturan.GantiBahasa();
+            Console.WriteLine($"New language / bahasa sekarang: {pengaturan.lang}" );
+        }
+
+        if (pengaturan.lang == "id")
+        {
+            Console.Write($"\n{pengaturan.lang} =  Masukkan jumlah uang yang akan di-transfer: ");
+            uang = Convert.ToInt32(Console.ReadLine());
+        }
+        else
+        {
+            Console.Write($"\n{pengaturan.lang} =  Please insert the amount of money to transfer: ");
+            uang = Convert.ToInt32(Console.ReadLine());
+        }
+
+        if (uang <= pengaturan.transfer.threshold || uang == pengaturan.transfer.threshold)
+        {
+            Console.Write($"biaya transfer adalah: {pengaturan.transfer.low_fee}");
+        }
+        else if(uang >= pengaturan.transfer.threshold)
+        {
+            Console.Write($"biaya transfer adalah: {pengaturan.transfer.high_fee}");
+        }
+
+        Console.WriteLine("");
+        int i = 1;
+        foreach (var data in pengaturan.methods)
+        {
+            
+            Console.WriteLine($"{i}. {data}");
+            i++;
+        }
+        string ya
+
+        if (pengaturan.lang == "id")
+        {
+            Console.Write($"\n{pengaturan.lang} => Ketik {pengaturan.confirmation.id}: ");
+            ya = Console.ReadLine();
+        }
+        else
+        {
+            Console.Write($"\n{pengaturan.lang} =>   Please type {pengaturan.confirmation.en}");
+            ya = Console.ReadLine();
+        }
+
+        if (ya == "ya")
+        {
+            Console.WriteLine("behasil transfer");
+        }
+        else {
+            Console.WriteLine("Sucsess transfer");
+        }
+    }
+}
